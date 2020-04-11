f54f756ab700066bfb4707bc16ff4eac668b904c
Lukas Schoerghuber <lukas.schoerghuber@posteo.at>
2020-04-11 18:05:28 +0200

    Initialize Spring Boot

---
+++ src/main/java/eu/lukasschoerghuber/publictransporthero/PublictransportheroApplication.java
@@ -0,0 +1,13 @@
+package eu.lukasschoerghuber.publictransporthero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublictransportheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublictransportheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/lukasschoerghuber/publictransporthero/PublictransportheroApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.lukasschoerghuber.publictransporthero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublictransportheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
